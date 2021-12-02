bd90712d3a27f519383a542b48fa736bb87c3817
abdurahmon <abdumovi@gmail.com>
2021-12-02 09:05:15 +0500

    end

---
+++ js/main.js
@@ -0,0 +1,79 @@
+let ravno = document.querySelector('.ravno');
+let input = document.querySelector('.input');
+let cler = document.querySelector('.cler');
+let allCler = document.querySelector('.clerall');
+let ul = document.querySelector('.list');
+let lst1 = [];
+
+function nums(...e) {
+  lst1.push(e);
+  input.value = lst1.join('');
+}
+
+function elem(e) {
+  if (input.value != '') {
+    if (lst1[lst1.length - 1] != '*' &&
+      lst1[lst1.length - 1] != '/' &&
+      lst1[lst1.length - 1] != '+' &&
+      lst1[lst1.length - 1] != '-') {
+      lst1.push(e);
+      input.value = lst1.join('');
+    }
+  }
+
+}
+cler.addEventListener('click', function () {
+  lst1.length = lst1.length - 1;
+  input.value = lst1.join('');
+
+});
+allCler.addEventListener('click', function () {
+  lst1.length = 0;
+  input.value = lst1.join('');
+
+});
+ravno.addEventListener('click', function (e) {
+  if (lst1[lst1.length - 1] != '*' &&
+    lst1[lst1.length - 1] != '/' &&
+    lst1[lst1.length - 1] != '+' &&
+    lst1[lst1.length - 1] != '-') {
+      let resul = lst1.join('');
+    if(resul.split('*').length > 1){
+      let all = eval(resul);
+      input.value = all;
+      let li = document.createElement('li');
+      li.className = 'list__item';
+      li.appendChild(document.createTextNode(resul +' = '+all));
+      ul.appendChild(li);
+    }
+    else if(resul.split('/').length > 1){
+      let all = eval(resul);
+      input.value = all;
+      let li = document.createElement('li');
+      li.className = 'list__item';
+      li.appendChild(document.createTextNode(resul +' = '+all));
+      ul.appendChild(li);
+    }
+    else if(resul.split('+').length > 1){
+      let all = eval(resul);
+      input.value = all;
+      let li = document.createElement('li');
+      li.className = 'list__item';
+      li.appendChild(document.createTextNode(resul +' = '+all));
+      ul.appendChild(li);
+    }
+    else if(resul.split('-').length > 1){
+      let all = eval(resul);
+      input.value = all;
+      let li = document.createElement('li');
+      li.className = 'list__item';
+      li.appendChild(document.createTextNode(resul +' = '+all));
+      ul.appendChild(li);
+    }
+    
+  }
+
+});
+allCler.addEventListener('doubleClick',function(){
+  
+});
